f932963b5f29c5b0cad78a0880ec2a0f841588eb
wwnn <1362948753@qq.com>
2025-07-20 14:31:54 +0800

    P7~P8

---
+++ P7~P8-TensorBoard的使用/test_tb.py
@@ -0,0 +1,18 @@
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+from PIL import Image
+
+writer = SummaryWriter("logs")
+# img_path = "D:\\code\\python\\土堆Pytorch\\P5-加载数据\\dataset\\train\\ants_image\\0013035.jpg"
+# img_path = "D:\\code\\python\\土堆Pytorch\\P5-加载数据\\dataset\\train\\bees_image\\16838648_415acd9e3f.jpg"
+img_path = "../P5-加载数据/dataset/train/bees_image/150013791_969d9a968b.jpg"
+img_PIL = Image.open(img_path)
+img_array = np.array(img_PIL)
+
+writer.add_image("train",img_array,1,dataformats='HWC')
+# writer.add_image()
+# y=2x
+for i in range(100):
+    writer.add_scalar("y=2x",3*i,i)
+
+writer.close()
